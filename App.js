4e59f0f7b74d2d919bf2b371a15b6072e71a5d62
phastandco <phastandco@gmail.com>
2022-07-04 16:33:17 +0200

    added NODE HOST env var

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import express from 'express'
 import routerEpisode from './routes/Episode.js'
 import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
-
+const HOST = process.env.NODE_HOST;
 const PORT = process.env.NODE_PORT;
+
 const app = express();
 
 app.get('/', (req, res) => {
